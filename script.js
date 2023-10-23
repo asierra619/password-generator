8993dc6a77f600f1d560a8998b2eb1941f358c59
Sierraaa <143159653+asierra619@users.noreply.github.com>
2023-10-22 22:11:39 -0700

    added variables for password options and window prompt/confirmations

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var Lowercase = ['a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'];
+var Uppercase = ['A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'];
+var Numbers = ['1,2,3,4,5,6,7,8,9,0'];
+var SpecialCharacters = ["!,@,#,$,%,^,&,*,(,),_,+,~,`,|,},{,[,],:,;,?,>,<,',',.,/,-,="];
+
+generateBtn.addEventListener('click', function() {
+  var passwordLength = prompt("Please choose a number of characters between 8 and 128.");
+
+  if (passwordLength <8 || passwordLength >128) {
+    return "Please pick a number between 8 and 128."
+  } else {
+    return "You have selected " + passwordLength + "characters."
+  }
+
+  includeLowercase = confirm("Would you like to use Lowercase letters?");
+
+  includeUppercase = confirm("Would you like to use Uppercase letters?");
+
+  includeNumber = confirm("Would you like to use Numbers?");
+
+  includeSpecialCharacters = confirm("Would you like to use Special Characters?");
+
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialCharacters) {
+    return "Please select at least one of the options to create your password."
+
+})
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,15 +38,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// og code
-// generateBtn.addEventListener("click", writePassword);
-// og code
-
-generateBtn.addEventListener('click', function() {
-  confirm("Would you like to include lowercase letters? \n OK or Cancel")
-  if (confirm === true) {
-    console.log("User wants lowercase characters in their password.")
-  } else {
-      console.log("User does NOT want lowercase characters in their password.")}
-  });
-  // continueConfirm("Would you like to include uppercase letters? \n OK or Cancel")
+generateBtn.addEventListener("click", writePassword);
